eb7cee0f6622ca282f129488670f9fc5199e3adf
Georgexzy <93133581+Georgexzy@users.noreply.github.com>
2025-08-24 18:06:53 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'quest-tracker-v8.0.0';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://sdk.scdn.co/spotify-player.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
